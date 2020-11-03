ae6ea56db43edb93d4b670b90bac8a444b88d4a3
nleond <nikleonidakis@gmail.com>
2020-11-03 18:42:00 +0200

    notify users on accepted datasets plus formatting

--- ckanext/userdatasets/logic/action/update.py
+++ ckanext/userdatasets/logic/action/update.py
@@ -8,7 +8,7 @@ from ckan.logic import check_access, get_action, ValidationError, NotFound
 import ckan.lib.base as base
 import ckan.logic as logic
 import ckan.model as m
-from ckan.common import _,c,  config
+from ckan.common import _, c,  config
 import ckan.lib.search as search
 import json
 import uuid
@@ -43,13 +43,14 @@ def package_update(context, data_dict):
         schema = package_plugin.update_package_schema()
 
     log.debug('package type %s', data_dict['type'])
-    
+
     # add dataset in corresponding topics(groups) based on subject
-    #add_to_topic()
+    # add_to_topic()
 
     # We modify the schema here to replace owner_org_validator by our own
     if 'owner_org' in schema:
-        schema['owner_org'] = [uds_oov if f is default_oov else f for f in schema['owner_org']]
+        schema['owner_org'] = [
+            uds_oov if f is default_oov else f for f in schema['owner_org']]
 
     if 'api_version' not in context:
         # check_data_dict() is deprecated. If the package_plugin has a
@@ -63,10 +64,10 @@ def package_update(context, data_dict):
                 # Old plugins do not support passing the schema so we need
                 # to ensure they still work.
                 package_plugin.check_data_dict(data_dict)
-    
+
     data, errors = lib_plugins.plugin_validate(
         package_plugin, context, data_dict, schema, 'package_update')
-    #log.debug('package_update validate_errs=%r user=%s package=%s data=%r',
+    # log.debug('package_update validate_errs=%r user=%s package=%s data=%r',
     #          errors, context.get('user'),
     #          context.get('package').name if context.get('package') else '',
     #          data)
@@ -82,7 +83,7 @@ def package_update(context, data_dict):
     else:
         rev.message = _(u'REST API: Update object %s') % data.get("name")
 
-    #avoid revisioning by updating directly
+    # avoid revisioning by updating directly
     model.Session.query(model.Package).filter_by(id=pkg.id).update(
         {"metadata_modified": datetime.datetime.utcnow()})
     model.Session.refresh(pkg)
@@ -93,8 +94,8 @@ def package_update(context, data_dict):
     context_org_update['ignore_auth'] = True
     context_org_update['defer_commit'] = True
     get_action('package_owner_org_update')(context_org_update,
-                                            {'id': pkg.id,
-                                             'organization_id': pkg.owner_org})
+                                           {'id': pkg.id,
+                                            'organization_id': pkg.owner_org})
 
     for item in plugins.PluginImplementations(plugins.IPackageController):
         item.edit(pkg)
@@ -114,30 +115,33 @@ def package_update(context, data_dict):
     # we could update the dataset so we should still be able to read it.
     context['ignore_auth'] = True
     output = data_dict['id'] if return_id_only \
-            else get_action('package_show')(context, {'id': data_dict['id']})
+        else get_action('package_show')(context, {'id': data_dict['id']})
 
     return output
 
-#Add new uuid for public doi (datasets are now published)
+# Add new uuid for public doi (datasets are now published)
+
+
 def add_public_doi(datasets):
     context_copy = {'model': m, 'session': m.Session,
-                   'user': c.user or c.author, 'auth_user_obj': c.userobj, 'ignore_auth' : True}
+                    'user': c.user or c.author, 'auth_user_obj': c.userobj, 'ignore_auth': True}
     for id in datasets:
         dataset = get_action('package_show')(context_copy, {'id': id})
-        if 'datacite.public_doi' not in dataset:   
+        if 'datacite.public_doi' not in dataset:
             doi = ext_helpers.getDataciteDoi(dataset)
             dataset['datacite.public_doi'] = doi
             package_update(context_copy, dataset)
-    return        
+    return
+
 
 def _bulk_update_dataset(context, data_dict, update_dict):
     ''' Bulk update shared code for organizations'''
 
     datasets = data_dict.get('datasets', [])
     org_id = data_dict.get('org_id')
-    
+
     log.info(' datasets are %s, type %s', datasets, type(datasets))
-        
+
     model = context['model']
     model.Session.query(model.package_table) \
         .filter(model.Package.id.in_(datasets)) \
@@ -192,6 +196,9 @@ def _bulk_update_dataset(context, data_dict, update_dict):
     # add after bulk updated for session conflicts
     if update_dict['private'] == False:
         add_public_doi(datasets)
+        # notify users for their accepted datasets
+        ext_helpers.notify_users(datasets)
+    
 
 
 def bulk_update_private(context, data_dict):
@@ -207,6 +214,7 @@ def bulk_update_private(context, data_dict):
     logic.check_access('bulk_update_private', context, data_dict)
     _bulk_update_dataset(context, data_dict, {'private': True})
 
+
 def bulk_update_public(context, data_dict):
     ''' Make a list of datasets public
 
@@ -221,31 +229,34 @@ def bulk_update_public(context, data_dict):
     _bulk_update_dataset(context, data_dict, {'private': False})
 
 
-def add_to_topic(context,data_dict):
-    if data_dict['type']!='harvest' and 'closed_tag' in data_dict:
+def add_to_topic(context, data_dict):
+    if data_dict['type'] != 'harvest' and 'closed_tag' in data_dict:
         groups = ext_helpers.topicsMatch(data_dict['closed_tag'])
-        
+
         context_group_update = context.copy()
         context_group_update['ignore_auth'] = True
         context_group_update['defer_commit'] = True
         for group in groups:
             group_data_dict = {"id": group,
-                                "object": data_dict['id'],
-                                "object_type": 'package',
-                                "capacity": 'public'}
+                               "object": data_dict['id'],
+                               "object_type": 'package',
+                               "capacity": 'public'}
             try:
-                logic.get_action('member_create')(context_group_update, group_data_dict)
+                logic.get_action('member_create')(
+                    context_group_update, group_data_dict)
             except NotFound:
-                abort(404, _('Group not found'))    
-        
-        if 'group_id' in data_dict and data_dict['group_id']:  
+                abort(404, _('Group not found'))
+
+        if 'group_id' in data_dict and data_dict['group_id']:
             #log.debug('group id %s',data_dict['group_id'])
             # add dataset in chosen community(group)
             community_data_dict = {"id": data_dict['group_id'],
-                                    "object": data_dict['id'],
-                                    "object_type": 'package',
-                                    "capacity": 'public'}
+                                   "object": data_dict['id'],
+                                   "object_type": 'package',
+                                   "capacity": 'public'}
             try:
-                logic.get_action('member_create')(context_group_update, community_data_dict)
+                logic.get_action('member_create')(
+                    context_group_update, community_data_dict)
             except NotFound:
-                    abort(404, _('Community not found'))       
+                abort(404, _('Community not found'))
+
